4f0f64fb2e5fefb7f814d7f2812ff069c579bf93
esoe <igor_nikulin@mail.ru>
2023-03-02 16:34:50 +0300

    qq

---
+++ src/main/java/ru/molokoin/ControllerRegistration.java
@@ -0,0 +1,41 @@
+package ru.molokoin;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControllerRegistration {
+    private FrameRegistration frame;
+    private CoverRequest request;
+    
+    public ControllerRegistration(){
+    }
+    public void preset(CoverRequest request){
+        request.setId(0);
+
+
+        // создание формы с заполненными предустановленными полями 
+        init(request);
+    }
+    public void init(CoverRequest request){
+        frame = new FrameRegistration();
+        /**
+         * добавляем обработчик нажатия на кнопку
+         * - вносим данные заполненных полей в базу
+         * - открываем окно добавления новых позиций в заказ
+         */
+        frame.getAddPositionButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //открываем окно добавления новых позиций в заказ
+                System.out.println("Добавить позицию к заказу ...");
+            }
+        });
+    }
+    /**
+     * Переносим данные внесенные пользователем
+     * - в обертку и затем
+     * - в базу
+     */
+    public void readRequest(){
+    }
+}
